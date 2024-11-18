1bc81264d3d52617bbded8abebe325d3eaf7b9bf
S2413017 <s2413017@bi.no>
2024-11-18 11:32:35 +0100

    final touches

--- GLMs_file.py
+++ GLMs_file.py
@@ -2,125 +2,102 @@ import numpy as np
 from scipy.stats import norm, bernoulli, poisson
 from scipy.optimize import minimize
 from sklearn.model_selection import train_test_split
-from data_loader import DataLoader, CSV, StatsModel
 
+## GeneralizedLinearModel takes input variables x and y to fit beta parameters and predict the μ.
+## @param x: matrix with predictor variables of shape (p+1, N)
+## @param y: matrix with dependent variables of shape (1, N)
 class GeneralizedLinearModel:
     def __init__(self, x, y):
-        # X should have the structure (p+1, N)
-        self._x = x
-        # Y should have the structure (1, N)
-        self._y = y
-        # X test should have the structure (p+1,N_test)
-        self._x_test = None
-        # Parameters should be a matrix with the shape (p+1,1)
-        self._params = None
-        # Mu should be a matrix of shape (1, N_train) or (1, N_test)
-        self._mu = None
+        self._x = x # shape (p+1, N) or (p+1, N_train)
+        self._y = y # shape (1, N) or (1, N_train)
+        self._x_test = None # shape (p+1, N_test)
+        self._params = None # shape (p+1, 1) or (p+1,)
+        self._mu = None # shape (1, N_train) or (1, N_test)
 
+    ## Splits dataset into train and test.
     def split(self):
-        # Splits dataset into train and test.
-        # X should be transposed into shape (N, p+1), Y into shape (N, 1).
+        # When splitting the dataset, we first transpose self._x and self._y to have shapes (N, p+1) and (N, 1).
         x_train, x_test, y_train, y_test = train_test_split(np.transpose(self._x), np.transpose(self._y), test_size=0.3, random_state=42)
-        # Change x and y to the training matrices.
-        # x_train has the shape (N_train, p+1), y_train has the shape (N_train, 1), x_test has the shape (N_test, p+1).
-        self._x = np.transpose(x_train)
-        self._y = np.transpose(y_train)
-        # Update the instance variable with the x_test matrix.
-        self._x_test = np.transpose(x_test)
+        # Update instance variables to match the train and test matrices.
+        self._x = np.transpose(x_train) # shape (N_train, p+1) -> (p+1, N_train)
+        self._y = np.transpose(y_train) # shape (N_train, 1) -> (1, N_train)
+        self._x_test = np.transpose(x_test) # shape (N_test, p+1) -> (p+1, N_test)
     
-    def negloglik(self):
+    ## Negloglik should be implemented in the subclasses (abstract method).
+    ## @param params: numpy array of beta parameters estimated in fit method
+    ## @param x: matrix of predictor variables
+    ## @param y: matrix of dependent variables
+    ## @return the negative log likelihood
+    def negloglik(self, params, x, y):
         raise NotImplementedError
     
+    ## Minimizes the negative log likelihood to estimate the unknown beta parameters.
+    ## @return list of estimated beta parameters
     def fit(self):
+        # Invokes split method to separate the data.
         self.split()
-        # Attributing the number of columns to num_params.
-        num_params = self._x.shape[0]
-        # Inital parameters have values 0.1 in all columns.
-        # Should have the shape (p+1,).
-        init_params = np.repeat(0.1, num_params)
-        # print(init_params.shape)
-        # Should have shape x=(p+1,N_train), y=(1,N_train)
+        num_params = self._x.shape[0] # number of columns in self._x
+        init_params = np.repeat(0.1, num_params) # 0.1 in all columns with the shape (p+1,)
+        # Minimize the negative log likelihood by inputting self._x and self._y and providing initial parameters.
         results = minimize(self.negloglik, init_params, args =(self._x,self._y))
-        # From a list create a numpy array with shape(p+1,1).
-        self._params = np.array(results['x']).reshape(len(results['x']),1)
-        print("Optimization is finished!\nThe estimated beta values are:", results['x'])
+        self._params = np.array(results['x']).reshape(len(results['x']),1) # from a list create a numpy array with shape(p+1,1)
+        print("\nOptimization is finished!\nThe estimated beta values are:", results['x'])
         return results['x']
     
+    ## Predicts the μ based on beta parameters, train matrices and the testing predictors.
+    ## @return the estimated μ parameters
     def predict(self):
+        # Invokes fit method to estimate beta parameters.
         self.fit()
+        # Invokes negloglik method to calculate μ (self._mu).
         self.negloglik(self._params,self._x_test,self._y)
         self._mu = np.asarray(self._mu).flatten()
         print(f"The estimated mean values are: {self._mu}")
         return self._mu
-    
+
+## Subclass specifies the negative log likelihood for a Normal distribution.
 class NormalDistr(GeneralizedLinearModel):
     def __init__(self, x, y):
         super().__init__(x, y)
 
     def negloglik(self, params, x, y):
         # Calculates eta by multiplying x transposed with beta parameters.
-        # Should be (N, p+1) x (p+1, 1)
-        new_x = np.transpose(x)
-        eta = np.matmul(new_x, params)
+        new_x = np.transpose(x) # shape (p+1, N) -> (N, p+1)
+        eta = np.matmul(new_x, params) # shape of params (p+1, 1)
         # Calculates mu using the link function.
-        # Should be (N, 1)
-        self._mu = eta
+        self._mu = eta # shape (N, 1)
         # Computes the log likelihood of the function.
         llik = np.sum(norm.logpdf(y, self._mu))
-        # Returns the negative log likelihood.
         return -llik
-
+    
+## Subclass specifies the negative log likelihood for a Poisson distribution.
 class PoissonDistr(GeneralizedLinearModel):
     def __init__(self, x, y):
         super().__init__(x, y)
 
     def negloglik(self, params, x, y):
         # Calculates eta by multiplying x transposed with beta parameters.
-        # Should be (N, p+1) x (p+1, 1)
-        new_x = np.transpose(x)
-        eta = np.matmul(new_x, params)
+        new_x = np.transpose(x) # shape (p+1, N) -> (N, p+1)
+        eta = np.matmul(new_x, params) # shape of params (p+1, 1)
         # Calculates mu using the link function.
-        # Should be (N, 1)
-        self._mu = np.exp(eta)
+        self._mu = np.exp(eta) # shape (N, 1)
         # Computes the log likelihood of the function.
         llik = np.sum(poisson.logpmf(y, self._mu))
         # Returns the negative log likelihood.
         return -llik
     
+## Subclass specifies the negative log likelihood for a Bernoulli distribution.
 class BernoulliDistr(GeneralizedLinearModel):
     def __init__(self, x, y):
         super().__init__(x, y)
 
     def negloglik(self, params, x, y):
         # Calculates eta by multiplying x transposed with beta parameters.
-        # Should be (N, p+1) x (p+1, 1)
-        new_x = np.transpose(x)
-        eta = np.matmul(new_x, params)
+        new_x = np.transpose(x) # shape (p+1, N) -> (N, p+1)
+        eta = np.matmul(new_x, params) # shape of params (p+1, 1)
         # Calculates mu using the link function.
-        # Should be (N, 1)
-        self._mu = np.exp(eta)/(1+np.exp(eta))
+        self._mu = np.exp(eta)/(1+np.exp(eta)) # shape (N, 1)
         # Computes the log likelihood of the function.
         llik = np.sum(bernoulli.logpmf(y, self._mu))
         # Returns the negative log likelihood.
         return -llik
-
-# # NORMAL TEST
-
-# norm_example = StatsModel('income', ['education','prestige'])
-# norm_example.readData("Duncan","carData")
-# norm_example = NormalDistr(norm_example.getX, norm_example.getY)
-# norm_example.predict()
-
-# # POISSON TEST
-
-# poiss_example = CSV("breaks", ["wool", "tension"])
-# poiss_example.readData("https://raw.githubusercontent.com/BI-DS/GRA-4152/refs/heads/master/warpbreaks.csv")
-# poiss_example = PoissonDistr(poiss_example.getX, poiss_example.getY)
-# poiss_example.predict()
-
-# # BERNOULLI TEST
-
-# bern_example = StatsModel("GRADE", ["GPA","TUCE","PSI"])
-# bern_example.readData("spector")
-# bern_example = BernoulliDistr(bern_example.getX, bern_example.getY)
-# bern_example.predict()
--- data_loader.py
+++ data_loader.py
@@ -6,11 +6,10 @@ from sklearn.model_selection import train_test_split
 
 
 ## Creates superclass DataLoader.
-#
 class DataLoader:
     ## Constructs instance variables.
-    # @ param y_col string with the name of the y variable
-    # @ param x_cols list with the names of the x variables
+    ## @ param y_col: string with the name of the y variable
+    ## @ param x_cols: list with the names of the x variables
     def __init__(self, y_col, x_cols):
         self._dataset = None
         self._y_col = y_col
@@ -18,12 +17,10 @@ class DataLoader:
         self._org_dataset = None
 
     ## Abstract method to be specified in the subclasses.
-    #
     def readData(self):
         raise NotImplementedError
     
     ## Organizes the dataset to only have the columns the user needs.
-    #
     def _organize(self):
         columns = []
         columns.append(self._y_col)
@@ -31,8 +28,7 @@ class DataLoader:
             columns.append(elem)
         self._org_dataset = self._dataset.loc[:, columns]
 
-    ## Returns a matrix of x.
-    #
+    ## @return matrix of x
     @property
     def x(self):
         if self._org_dataset is None:
@@ -40,30 +36,26 @@ class DataLoader:
         X = matrix(self._org_dataset.iloc[:,1:].to_numpy())
         return X
     
-    ## Adds a constant (an intercept term) to the model.
-    #
+    ## @return matrix of x with a constant (an intercept term)
     @property
     def _add_constant(self):
         X_const = np.insert(self.x, 0, 1, axis=1)
         return X_const
     
 
-    ## Allows the user to access the matrix of X.
-    #
+    ## Allows the user to access the matrix of x.
     @property
     def getX(self):
         X = self._add_constant
         return np.transpose(X)
     
-    ## Transposes the matrix of X.
-    #
+    ## Allows the user to access the transposed matrix of x.
     @property
     def getXt(self):
         Xt = (self._add_constant)
         return Xt
 
     ## Allows the user to access the matrix of Y.
-    #
     @property
     def getY(self):
         if self._org_dataset is None:
@@ -71,16 +63,19 @@ class DataLoader:
         Y = matrix(self._org_dataset.iloc[:,0].to_numpy())
         return Y
     
-    ## Transposes the matrix of Y.
+    ## Allows the user to access the transposed matrix of y.
     @property
     def getYt(self):
         Yt = self.getY.transpose()
         return Yt
 
+## Enables the object to fetch data from statsmodel datasets.
 class StatsModel(DataLoader):
     def __init__(self, y_col, x_cols):
         super().__init__(y_col, x_cols)
 
+    ## @param data_name: name of dataset
+    ## @param package_name: name of R dataset; if not specified, program searches through built-in datasets
     def readData(self, data_name, package_name=None):
         try:
             ## Handles specific R datasets by package name and dataset name.
@@ -95,9 +90,11 @@ class StatsModel(DataLoader):
         except Exception as e:
             print(f"Error reading data: {e}")
 
+## Enables the object to fetch data from a CSV dataset.
 class CSV(DataLoader):
     def __init__(self, y_col, x_cols):
         super().__init__(y_col, x_cols)
     
+    ## @param file_path: URL to the designated file
     def readData(self, file_path):
         self._dataset = pd.read_csv(file_path)
